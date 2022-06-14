1f0921d2bde674fd77ee1dfd5f1dc85ecb879eff
Neil Buckley <nmb88@hotmail.com>
2022-06-14 11:47:39 +0100

    add Nav component and basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header.jsx';
+import Nav from './components/Nav.jsx';
 
 function App() {
   return (
     <div className="App">
         <Header />
+        <Nav />
     </div>
   );
 }
