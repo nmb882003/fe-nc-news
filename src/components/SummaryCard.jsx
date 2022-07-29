851fb9bc18128cf4cb77eca8fd194fc34a098fd3
Neil Buckley <nmb88@hotmail.com>
2022-07-29 21:08:23 +0100

    further improve styling of SummaryCard component

--- src/components/SummaryCard.jsx
+++ src/components/SummaryCard.jsx
@@ -3,12 +3,12 @@ const SummaryCard = ({ summary }) => {
     return (
         <>
             <h2 className="summarycard-heading">{summary.title}</h2>
-            <div className="summarycard-body">
+            <div className="summarycard-details">
                 <p>{summary.author}</p>
-                <p>{summary.created_at}</p>
+                <p>{summary.comment_count} v: {summary.votes}</p>
             </div>
-            <div className="summarycard-body">
-                <p>c: {summary.comment_count} v: {summary.votes}</p>
+            <div className="summarycard-details">
+                <p>{summary.created_at}</p>
                 <p>In topic: {summary.topic}</p>
             </div>
         </>
