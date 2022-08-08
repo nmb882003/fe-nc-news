a51e4e4139bf23071dd159b53b732e83927a43cc
Neil Buckley <nmb88@hotmail.com>
2022-08-08 10:46:27 +0100

    fixes annoying style inconsistency with SortBar buttons (ugh)

--- src/components/SortBar.jsx
+++ src/components/SortBar.jsx
@@ -15,9 +15,9 @@ const SortBar = ({ sortDirection, setSortDirection, selectedButton, setSelectedB
         <div className="sortbar-outer">
             <div className="sortbar-inner">
                 <p className="sortbar-description">Sort articles by</p>
-                <button type="button" className={selectedButton === 'created_at' ? "sortbar-inner-btn-selected" : "sortbar-inner-btn"} onClick={() => updateButton('created_at', selectedButton, sortDirection)}>Date {selectedButton === 'created_at' ? sortDirection === 'asc' ? '🔺' : '🔻' : ''}</button>
-                <button type="button" className={selectedButton === 'comment_count' ? "sortbar-inner-btn-selected" : "sortbar-inner-btn"} onClick={() => updateButton('comment_count', selectedButton, sortDirection)}>Comment count {selectedButton === 'comment_count' ? sortDirection === 'asc' ? '🔺' : '🔻' : ''}</button>
-                <button type="button" className={selectedButton === 'votes' ? "sortbar-inner-btn-selected" : "sortbar-inner-btn"} onClick={() => updateButton('votes', selectedButton, sortDirection)}>Votes {selectedButton === 'votes' ? sortDirection === 'asc' ? '🔺' : '🔻' : ''}</button>
+                <button type="button" className="sortbar-inner-btn" onClick={() => updateButton('created_at', selectedButton, sortDirection)}>Date {selectedButton === 'created_at' ? sortDirection === 'asc' ? '🔺' : '🔻' : ''}</button>
+                <button type="button" className="sortbar-inner-btn" onClick={() => updateButton('comment_count', selectedButton, sortDirection)}>Comment count {selectedButton === 'comment_count' ? sortDirection === 'asc' ? '🔺' : '🔻' : ''}</button>
+                <button type="button" className="sortbar-inner-btn" onClick={() => updateButton('votes', selectedButton, sortDirection)}>Votes {selectedButton === 'votes' ? sortDirection === 'asc' ? '🔺' : '🔻' : ''}</button>
             </div>
         </div>
     )
