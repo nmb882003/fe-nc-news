01188c284a1b442beb6ac54a88309a56a1d6b704
Neil Buckley <nmb88@hotmail.com>
2022-08-08 02:42:54 +0100

    refactor SortBar to improve code legibility

--- src/components/SortBar.jsx
+++ src/components/SortBar.jsx
@@ -3,19 +3,21 @@ const SortBar = ({ sortDirection, setSortDirection, selectedButton, setSelectedB
         sortDirection === 'asc' ? setSortDirection('desc') : setSortDirection('asc');
     }
 
+    const updateButton = (name, selectedButton, sortDirection) => {
+        if (selectedButton === name) {
+            updateSortDirection(sortDirection)
+        } else {
+            setSelectedButton(name);
+        }
+    }
+
     return (
         <div className="sortbar-outer">
             <div className="sortbar-inner">
                 <p className="sortbar-description">Sort articles by</p>
-                <button type="button" className={selectedButton === 'created_at' ? "sortbar-inner-btn-selected" : "sortbar-inner-btn"} onClick={() => {
-                    selectedButton === 'created_at' ? updateSortDirection(sortDirection) : setSelectedButton('created_at');
-                }}>Date {selectedButton === 'created_at' ? sortDirection === 'asc' ? '🔺' : '🔻' : ''}</button>
-                <button type="button" className={selectedButton === 'comment_count' ? "sortbar-inner-btn-selected" : "sortbar-inner-btn"} onClick={() => {
-                    selectedButton === 'comment_count' ? updateSortDirection(sortDirection) : setSelectedButton('comment_count');
-                }}>Comment count {selectedButton === 'comment_count' ? sortDirection === 'asc' ? '🔺' : '🔻' : ''}</button>
-                <button type="button" className={selectedButton === 'votes' ? "sortbar-inner-btn-selected" : "sortbar-inner-btn"} onClick={() => {
-                    selectedButton === 'votes' ? updateSortDirection(sortDirection) : setSelectedButton('votes');
-                }}>Votes {selectedButton === 'votes' ? sortDirection === 'asc' ? '🔺' : '🔻' : ''}</button>
+                <button type="button" className={selectedButton === 'created_at' ? "sortbar-inner-btn-selected" : "sortbar-inner-btn"} onClick={() => updateButton('created_at', selectedButton, sortDirection)}>Date {selectedButton === 'created_at' ? sortDirection === 'asc' ? '🔺' : '🔻' : ''}</button>
+                <button type="button" className={selectedButton === 'comment_count' ? "sortbar-inner-btn-selected" : "sortbar-inner-btn"} onClick={() => updateButton('comment_count', selectedButton, sortDirection)}>Comment count {selectedButton === 'comment_count' ? sortDirection === 'asc' ? '🔺' : '🔻' : ''}</button>
+                <button type="button" className={selectedButton === 'votes' ? "sortbar-inner-btn-selected" : "sortbar-inner-btn"} onClick={() => updateButton('votes', selectedButton, sortDirection)}>Votes {selectedButton === 'votes' ? sortDirection === 'asc' ? '🔺' : '🔻' : ''}</button>
             </div>
         </div>
     )
