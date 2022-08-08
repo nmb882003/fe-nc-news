6e6cbfcae10443fc293862bc77bc4f24c639faa4
Neil Buckley <nmb88@hotmail.com>
2022-08-08 08:10:00 +0100

    add error handling for non-existent topic

--- src/components/SummaryList.jsx
+++ src/components/SummaryList.jsx
@@ -3,12 +3,14 @@ import { useParams } from 'react-router-dom';
 import SummaryCard from './SummaryCard.jsx';
 import SortBar from './SortBar.jsx';
 import PagBar from './PagBar.jsx';
+import ErrorMessage from './ErrorMessage.jsx';
 
 const SummaryList = () => {
     const [summaryList, setSummaryList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [sortDirection, setSortDirection] = useState("desc");
     const [selectedButton, setSelectedButton] = useState("created_at");
+    const [error, setError] = useState(null);
 
     const { topic } = useParams();
 
@@ -23,31 +25,41 @@ const SummaryList = () => {
 
         setIsLoading(true);
         fetch(path)
-
-            .then(res => res.json())
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                }
+                else {
+                    return Promise.reject({ status: res.status, msg: res.statusText });
+                }
+            })
 
             .then(({ articles }) => {
                 setSummaryList(articles);
                 setIsLoading(false);
             })
+
+            .catch(err => setError(err));
+
     }, [topic, sortDirection, selectedButton])
 
     return (
         <>
-            {isLoading ? <p>Please wait while article data loads... </p> :
-                <div className="summarylist-container">
-                    <SortBar sortDirection={sortDirection} setSortDirection={setSortDirection} selectedButton={selectedButton} setSelectedButton={setSelectedButton}/>
-                    <ul className="summarylist">
-                        {summaryList.map(summary => {
-                            return (
-                                <li key={summary.article_id} className="summarycard">
-                                    <SummaryCard summary={summary} />
-                                </li>
-                            )
-                        })}
-                    </ul>
-                    <PagBar />
-                </div>
+            {error ? <ErrorMessage msg={error.msg} code={error.status} /> :
+                isLoading ? <p>Please wait while article data loads... </p> :
+                    <div className="summarylist-container">
+                        <SortBar sortDirection={sortDirection} setSortDirection={setSortDirection} selectedButton={selectedButton} setSelectedButton={setSelectedButton} />
+                        <ul className="summarylist">
+                            {summaryList.map(summary => {
+                                return (
+                                    <li key={summary.article_id} className="summarycard">
+                                        <SummaryCard summary={summary} />
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                        <PagBar />
+                    </div>
             }
         </>
     )
