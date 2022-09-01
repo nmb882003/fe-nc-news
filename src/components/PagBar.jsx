46043e8745a85d08931a2290111a0e6f916f1c78
Neil Buckley <nmb88@hotmail.com>
2022-09-01 05:39:04 +0100

    temporarily disable 'load more' button in PagBar component to prevent spamming by user

--- src/components/PagBar.jsx
+++ src/components/PagBar.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import { getSummaries } from "../utils/api";
 
 const PagBar = ({ resultsEnd, setResultsEnd, totalResults, currentPage, setCurrentPage, setSummaryList, selectedButton, sortDirection, topic }) => {
+    const [disableButton, setDisableButton] = useState(false);
 
     const showMoreResults = () => {
         let path = `https://neilb-nc-news-server.herokuapp.com/api/articles?sort_by=${selectedButton}&order=${sortDirection}&p=${currentPage + 1}`;
 
         if (topic) path += `&topic=${topic}`;
+        
+        setDisableButton(true);
 
         getSummaries(path)
             .then(res => res.json())
@@ -14,6 +18,7 @@ const PagBar = ({ resultsEnd, setResultsEnd, totalResults, currentPage, setCurre
                 setSummaryList(currentList => [...currentList, ...articles]);
                 setCurrentPage(currentPage + 1);
                 setResultsEnd(resultsEnd + articles.length);
+                setDisableButton(false);
             })
 
     }
@@ -21,7 +26,7 @@ const PagBar = ({ resultsEnd, setResultsEnd, totalResults, currentPage, setCurre
     return (
         <div className="pagbar">
             <p className="pagbar-text">Showing results 1 - {resultsEnd} of {totalResults}</p>
-            <button onClick={() => showMoreResults()} disabled={resultsEnd === totalResults ? true : false}>Show more</button>
+            <button onClick={() => showMoreResults()} disabled={resultsEnd === totalResults ? true : disableButton ? true : false}>Show more</button>
         </div>
     )
 }
